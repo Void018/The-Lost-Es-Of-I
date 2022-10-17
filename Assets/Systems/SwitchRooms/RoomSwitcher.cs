e868b3e1ec3f442ec0ce1ce76de92701d59c5986
ahmed-alnour123 <ahmed2699@gmail.com>
2022-10-17 18:26:05 +0200

    worked on level1

--- Assets/Systems/SwitchRooms/RoomSwitcher.cs
+++ Assets/Systems/SwitchRooms/RoomSwitcher.cs
@@ -1,17 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
 using Cinemachine;
 using UnityEngine;
 
 public class RoomSwitcher : MonoBehaviour {
-    public Transform targetPosition;
+    public Transform targetRoom;
 
+    private CinemachineVirtualCamera targetCamera;
     private CinemachineVirtualCamera cam;
     private Transform player;
-    private CinemachineVirtualCamera targetCamera;
+    private Transform targetPosition;
+    private List<CinemachineVirtualCamera> allCams;
 
     void Start() {
         cam = transform.parent.GetComponentInChildren<CinemachineVirtualCamera>();
         player = FindObjectOfType<PlayerController>().transform;
-        targetCamera = targetPosition.parent.GetComponentInChildren<CinemachineVirtualCamera>();
+        targetPosition = transform.GetChild(0);
+        allCams = FindObjectsOfType<CinemachineVirtualCamera>().ToList();
+        targetCamera = targetRoom.GetComponentInChildren<CinemachineVirtualCamera>();
     }
 
     void Update() {
@@ -22,8 +28,10 @@ public class RoomSwitcher : MonoBehaviour {
         if (!other.CompareTag("Player")) return;
 
         player.transform.position = targetPosition.position;
-        Debug.Log(targetPosition.name);
-        cam.enabled = false;
+        if (cam == targetCamera) return;
+
+        print($"current: {cam.transform.parent.name}, target: {targetCamera.transform.parent.name}");
+        allCams.ForEach(c => c.enabled = false);
         targetCamera.enabled = true;
     }
 
